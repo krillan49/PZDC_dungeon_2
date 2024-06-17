3f71166bc473ace0765a1f95b4533b38d5786293
krillan <pushkinidze49@gmail.com>
2024-06-17 11:36:39 +0300

    hero info stats mehtod add

--- info_block.rb
+++ info_block.rb
@@ -1,4 +1,24 @@
 module InfoBlock
+  def InfoBlock.hero_stats_info(hero)
+    puts '--------------------------------------------------------------------------------------------'
+    puts '--------------------------------------------------------------------------------------------'
+    puts "#{hero.name}"
+    puts "Уровень #{hero.lvl} (#{hero.exp}/#{hero.exp_lvl[hero.lvl + 1]})"
+    puts "Н А В Ы К И:"
+    puts "[акт] #{hero.active_skill.name} #{hero.active_skill.description}"
+    puts "[пас] #{hero.passive_skill.name} #{hero.passive_skill.description}"
+    puts "[неб] #{hero.camp_skill.name} #{hero.camp_skill.description}"
+    puts "С Т А Т Ы:"
+    puts "HP #{hero.hp.round}/#{hero.hp_max} Реген #{hero.regen_hp_base} Восстановление #{hero.recovery_hp.round}"
+    puts "MP #{hero.mp.round}/#{hero.mp_max} Реген #{hero.regen_mp_base} Восстановление #{hero.recovery_mp.round}"
+    puts "Урон #{hero.min_dmg}-#{hero.max_dmg} (базовый #{hero.min_dmg_base}-#{hero.max_dmg_base} + #{hero.weapon.name} #{hero.weapon.min_dmg}-#{hero.weapon.max_dmg})"
+    puts "Точность #{hero.accuracy} (базовая #{hero.accuracy_base} + #{hero.arms_armor.name} #{hero.arms_armor.accuracy})"
+    puts "Броня #{hero.armor} (базовая #{hero.armor_base} + #{hero.body_armor.name} #{hero.body_armor.armor} + #{hero.head_armor.name} #{hero.head_armor.armor} + #{hero.arms_armor.name} #{hero.arms_armor.armor} + #{hero.shield.name} #{hero.shield.armor})"
+    puts "Шанс блока #{hero.block_chance} (#{hero.shield.name} #{hero.shield.block_chance}) блокируемый урон #{hero.block_power_in_percents}%"
+    puts '--------------------------------------------------------------------------------------------'
+    puts '--------------------------------------------------------------------------------------------'
+  end
+
   def InfoBlock.enemy_start_stats_info(enemy)
     puts "В бой! Ваш противник #{enemy.name}"
     puts "HP #{enemy.hp}"
@@ -8,3 +28,19 @@ module InfoBlock
     puts "Block #{enemy.block_chance} = #{enemy.shield.block_chance}(#{enemy.shield.name})"
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- new_main.rb
+++ new_main.rb
@@ -7,30 +7,6 @@ require_relative "info_block"
 require_relative "arts"
 
 
-#======================================= Методы временные решения =================================================
-
-# Панель характеристик персонажа
-def character_panel
-  puts '--------------------------------------------------------------------------------------------'
-  puts '--------------------------------------------------------------------------------------------'
-  puts "#{@hero.name}"
-  puts "Уровень #{@hero.lvl} (#{@hero.exp}/#{@hero.exp_lvl[@hero.lvl + 1]})"
-  puts "Н А В Ы К И:"
-  puts "[акт] #{@hero.active_skill.name} #{@hero.active_skill.description}"
-  puts "[пас] #{@hero.passive_skill.name} #{@hero.passive_skill.description}"
-  puts "[неб] #{@hero.camp_skill.name} #{@hero.camp_skill.description}"
-  puts "С Т А Т Ы:"
-  puts "HP #{@hero.hp.round}/#{@hero.hp_max} Реген #{@hero.regen_hp_base} Восстановление #{@hero.recovery_hp.round}"
-  puts "MP #{@hero.mp.round}/#{@hero.mp_max} Реген #{@hero.regen_mp_base} Восстановление #{@hero.recovery_mp.round}"
-  puts "Урон #{@hero.min_dmg}-#{@hero.max_dmg} (базовый #{@hero.min_dmg_base}-#{@hero.max_dmg_base} + #{@hero.weapon.name} #{@hero.weapon.min_dmg}-#{@hero.weapon.max_dmg})"
-  puts "Точность #{@hero.accuracy} (базовая #{@hero.accuracy_base} + #{@hero.arms_armor.name} #{@hero.arms_armor.accuracy})"
-  puts "Броня #{@hero.armor} (базовая #{@hero.armor_base} + #{@hero.body_armor.name} #{@hero.body_armor.armor} + #{@hero.head_armor.name} #{@hero.head_armor.armor} + #{@hero.arms_armor.name} #{@hero.arms_armor.armor} + #{@hero.shield.name} #{@hero.shield.armor})"
-  puts "Шанс блока #{@hero.block_chance} (#{@hero.shield.name} #{@hero.shield.block_chance}) блокируемый урон #{@hero.block_power_in_percents}%"
-  puts '--------------------------------------------------------------------------------------------'
-  puts '--------------------------------------------------------------------------------------------'
-end
-
-#==================================================================================================================
 
 # Создание персонажа..............................................................................................
 puts 'Создание персонажа'
@@ -106,7 +82,7 @@ while true
   # распределение очков характеристик --------------------------------------------------------------------------
   while @hero.stat_points != 0
 
-    character_panel # Панель характеристик персонажа (Основные)
+    InfoBlock.hero_stats_info(@hero) # Панель характеристик персонажа
 
     distribution = ''
     until %w[H M X A].include?(distribution)
@@ -134,7 +110,7 @@ while true
   # распределение очков навыков --------------------------------------------------------------------------
   while @hero.skill_points != 0
 
-    character_panel # Панель характеристик персонажа (Основные)
+    InfoBlock.hero_stats_info(@hero) # Панель характеристик персонажа
 
     distribution = ''
     while distribution != 'S' and distribution != 'P' and distribution != 'N'
@@ -153,7 +129,7 @@ while true
     end
   end
 
-  character_panel # Панель характеристик персонажа (Основные)
+  InfoBlock.hero_stats_info(@hero) # Панель характеристик персонажа
 
   #---------------------------------------------------------------------------------
 
