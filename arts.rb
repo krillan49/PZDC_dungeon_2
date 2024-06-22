7c0825cf7951248389b7f099a5a467dc4dca5b53
krillan <pushkinidze49@gmail.com>
2024-06-22 14:13:38 +0300

    update to MVC-like

--- arts.rb
+++ arts.rb
@@ -2,7 +2,7 @@ require 'yaml'
 
 module Art
   def Art.display_art(name)
-    art = YAML.safe_load_file('data/arts/arts.yml', symbolize_names: true)[name]
+    art = YAML.safe_load_file('graphics/arts/arts.yml', symbolize_names: true)[name]
     puts art
   end
 end
