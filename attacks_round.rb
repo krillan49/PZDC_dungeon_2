d3842105e6b0cc445546c30c4e169e4779541083
krillan <pushkinidze49@gmail.com>
2024-06-24 11:27:29 +0300

    add attack round class

---
+++ attacks_round.rb
@@ -0,0 +1,209 @@
+class AttacksRound
+  def initialize(hero, enemy)
+    @hero = hero
+    @hero_damage = rand(hero.min_dmg..hero.max_dmg)
+    @hero_accuracy = @hero.accuracy
+    @hero_block_successful = @hero.block_chance >= rand(1..100)
+
+    @enemy = enemy
+    @enemy_damage = rand(enemy.min_dmg..enemy.max_dmg)
+    @enemy_accuracy = @enemy.accuracy
+    @enemy_block_successful = @enemy.block_chance >= rand(1..100)
+  end
+
+  def action
+    hero_select_type_of_attack()
+    enemy_select_type_of_attack()
+    count_hero_final_damage()
+    count_enemy_final_damage()
+    hero_attack_effects()
+    enemy_attack_effects()
+    @hero.regeneration_hp_mp # регенерация
+    round_result()
+  end
+
+  def hero_run?
+    if @hero.hp < (@hero.hp_max * 0.15) && @hero.hp > 0 && @enemy.hp > 0
+      print 'Ты на пороге смерти. Попытаться убежать? (y/N) : '
+      run_select = gets.strip.upcase
+      if run_select == 'Y'
+        if rand(0..2) >= 1
+          puts "Сбежал ссыкло"
+          return true
+        else
+          @hero.hp -= @enemy_damage
+          puts "Не удалось убежать #{@enemy.name} нанес #{@enemy_damage.round} урона"
+          if @hero.hp <= 0
+            puts "Ты убит - трусливая псина!"
+            Art.display_art(:game_over)
+            exit
+          end
+        end
+      end
+    end
+    false
+  end
+
+  private
+
+  def hero_select_type_of_attack
+    success = false # для проверки возможно ли проведение выбранной атаки
+    until success
+      print 'Атакуйте! 1.По телу(B) 2.По голове(H) 3.По ногам(L) 4.Навык(S) '
+      selected_type = gets.strip.upcase
+      success = case selected_type
+      when 'H'; hero_head_attack_type?()
+      when 'L'; hero_legs_attack_type?()
+      when 'S'; hero_skill_attack_type?()
+      else; hero_body_attack_type?()
+      end
+    end
+  end
+  def hero_body_attack_type?
+    @hero_attack_type = "по телу"
+    true
+  end
+  def hero_head_attack_type?
+    @hero_damage *= 1.5
+    @hero_accuracy *= 0.7
+    @hero_attack_type = "по голове"
+    true
+  end
+  def hero_legs_attack_type?
+    @hero_damage *= 0.7
+    @hero_accuracy *= 1.5
+    @hero_attack_type = "по ногам"
+    true
+  end
+  def hero_skill_attack_type?
+    if @hero.mp >= @hero.active_skill.mp_cost
+      @hero_damage *= @hero.active_skill.damage_mod
+      @hero_accuracy *= @hero.active_skill.accuracy_mod
+      @hero_attack_type = @hero.active_skill.name
+      @hero.mp -= @hero.active_skill.mp_cost
+      true
+    else
+      puts "Недостаточно маны на #{@hero.active_skill.name}"
+      false
+    end
+  end
+  #
+  def enemy_select_type_of_attack
+    selected_type = rand(1..10)
+    case selected_type
+    when 1; enemy_head_attack_type()
+    when 2; enemy_legs_attack_type()
+    else; enemy_body_attack_type()
+    end
+  end
+  def enemy_body_attack_type
+    @enemy_attack_type = "по телу"
+  end
+  def enemy_head_attack_type
+    @enemy_damage *= 1.5
+    @enemy_accuracy *= 0.7
+    @enemy_attack_type = "по голове"
+  end
+  def enemy_legs_attack_type
+    @enemy_damage *= 0.7
+    @enemy_accuracy *= 1.5
+    @enemy_attack_type = "по ногам"
+  end
+
+  def count_hero_final_damage
+    hero_damage_reduced_by_enemy_block()
+    hero_damage_reduced_by_enemy_armor()
+  end
+  def hero_damage_reduced_by_enemy_block
+    @hero_damage /= @enemy.block_power_coeff if @enemy_block_successful
+  end
+  def hero_damage_reduced_by_enemy_armor
+    @hero_damage -= @enemy.armor
+    @hero_damage = 0 if @hero_damage < 0
+  end
+  #
+  def count_enemy_final_damage
+    enemy_damage_reduced_by_hero_block()
+    enemy_damage_reduced_by_hero_armor()
+  end
+  def enemy_damage_reduced_by_hero_block
+    @enemy_damage /= @hero.block_power_coeff if @hero_block_successful
+  end
+  def enemy_damage_reduced_by_hero_armor
+    @enemy_damage -= @hero.armor
+    @enemy_damage = 0 if @enemy_damage < 0
+  end
+
+  def hero_attack_effects
+    @hero_hit = @hero_accuracy >= rand(1..100)
+    hero_hit_or_miss()
+    hero_hit_passive_slill_effects()
+  end
+  def hero_hit_or_miss
+    if @hero_hit
+      puts "#{@enemy.name} заблокировал #{@enemy.block_power_in_percents}% урона" if @enemy_block_successful
+      @enemy.hp -= @hero_damage
+      puts "Вы нанесли #{@hero_damage.round} урона #{@hero_attack_type}"
+    else
+      puts "Вы промахнулись #{@hero_attack_type}"
+    end
+  end
+  def hero_hit_passive_slill_effects
+    case @hero.passive_skill.name
+    when "Ошеломление"
+      if @hero_hit && @hero_damage * @hero.passive_skill.accuracy_reduce_coef > (@enemy.hp + @hero_damage) / 2 # прибавляется дамаг который отнялся выше для подсчета эффекта ошеломления
+        @enemy_accuracy *= 0.1 * rand(1..9)
+        puts "атака ошеломила врага, уменьшив его точность до #{(@enemy.accuracy * 0.1 * rand(1..9)).round}"
+      end
+    when "Концентрация"
+      damage_bonus = @hero.passive_skill.damage_bonus # чтобы далее был одинаковый
+      if @hero_hit && damage_bonus > 0
+        @enemy.hp -= damage_bonus
+        puts "дополнительный урон от концентрации #{damage_bonus.round(1)}"
+      end
+    end
+  end
+  #
+  def enemy_attack_effects
+    @enemy_hit = @enemy_accuracy >= rand(1..100)
+    enemy_hit_or_miss()
+  end
+  def enemy_hit_or_miss
+    if @enemy_hit
+      puts "Вы заблокировали #{@hero.block_power_in_percents}% урона" if @hero_block_successful
+      @hero.hp -= @enemy_damage
+      puts "#{@enemy.name} нанес #{@enemy_damage.round} урона #{@enemy_attack_type}"
+    else
+      puts "#{@enemy.name} промахнулся #{@enemy_attack_type}"
+    end
+  end
+
+  def round_result
+    puts "У вас осталось #{@hero.hp.round}/#{@hero.hp_max} жизней и #{@hero.mp.round}/#{@hero.mp_max} выносливости, у #{@enemy.name}а осталось #{@enemy.hp.round} жизней."
+    if @hero.hp <= 0
+      puts "Ты убит - слабак!"
+      Art.display_art(:game_over)
+      exit
+    elsif @enemy.hp <= 0
+      puts "#{@enemy.name} убит, победа!!!"
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require_relative "hero_creator"
 require_relative "hero_updator"
 require_relative "enemy_creator"
+require_relative "attacks_round"
 require_relative "hero"
 require_relative "skills"
 require_relative "enemyes"
 require_relative "weapons"
 require_relative "loot"
+require_relative "menues"
 require_relative "info_block"
 require_relative "arts"
 
@@ -13,22 +15,20 @@ require_relative "arts"
 
 @hero = HeroCreator.new.create_new_hero # Создание нового персонажа
 
-
-# Основной игровой блок ===============================================================================================
+# Основной игровой блок
 leveling = 0
 while true
 
   HeroUpdator.new(@hero).spend_stat_points # распределение очков характеристик
-  HeroUpdator.new(@hero).spend_skill_points # распределение очков навыков
+  HeroUpdator.new(@hero).spend_skill_points # распределение очков навыков  (тут вызывается старое меню, потом доделать)
 
-  InfoBlock.hero_stats_info(@hero) # Панель характеристик персонажа
-
-  #---------------------------------------------------------------------------------
+  # Характеристики персонажа
+  puts InfoBlock.hero_name_level_exp(@hero)
+  Menu.new(:character_stats, @hero).display
+  puts InfoBlock.character_skills(@hero)
 
   @hero.use_camp_skill # Навык Первая помощь
   @hero.rest # пассивное восстановления жизней и маны между боями
-
-  #--------------------------------------------------------------------------------------------------------------
 
   print 'Чтобы начать следующий бой нажмите Enter'
   gets
@@ -36,191 +36,37 @@ while true
 
   @enemy = EnemyCreator.new(leveling).create_new_enemy # Назначение противника
 
-  InfoBlock.enemy_start_stats_info(@enemy)
+  # Характеристики противника
+  puts InfoBlock.enemy_name(@enemy)
+  Menu.new(:character_stats, @enemy).display
 
-  # Ход боя ===============================================================================================
+  # Ход боя
   run = false
   lap = 1 # номер хода
-
-  while @enemy.hp > 0 and run == false
-
+  while @enemy.hp > 0 && run == false
     puts "====================================== ХОД #{lap} ============================================"
 
-    # Расчет базового урона----------------------------------------------------------------------------------------
-    damage_pl = rand(@hero.min_dmg..@hero.max_dmg)
-
-    damage_en = rand(@enemy.min_dmg..@enemy.max_dmg)
-    #----------------------------------------------------------------------------------------------------------
-
-    # Выбор вида атаки ------------------------------------------------------------------------------------
-    cant_do = 0
-    while cant_do == 0
-      cant_do += 1
-      print 'Атакуйте! 1.По телу(B) 2.По голове(H) 3.По ногам(L) 4.Навык(S) '
-      target_pl = gets.strip.upcase
-      target_name_pl = "по телу"
-      case target_pl
-      when 'H'
-        damage_pl *= 1.5
-        accuracy_action_pl = @hero.accuracy * 0.7
-        target_name_pl = "по голове"
-      when 'L'
-        damage_pl *= 0.7
-        accuracy_action_pl = @hero.accuracy * 1.5
-        target_name_pl = "по ногам"
-      when 'S'
-        if @hero.mp >= @hero.active_skill.mp_cost
-          damage_pl *= @hero.active_skill.damage_mod
-          accuracy_action_pl = @hero.accuracy * @hero.active_skill.accuracy_mod
-          target_name_pl = @hero.active_skill.name
-          @hero.mp -= @hero.active_skill.mp_cost
-        else
-          puts "Недостаточно маны на #{@hero.active_skill.name}"
-          cant_do -= 1
-        end
-      else
-        accuracy_action_pl = @hero.accuracy
-      end
-    end
-    # -----------------------------------------------------------------------------------------------------
-
-    # Направление атаки бота ----------------------------------------------------------------------------
-    target_en = rand(1..10)
-    name_target_en = "по телу"
-    if target_en >= 1 and target_en <= 3
-      damage_en *= 1.5
-      accurasy_action_en = @enemy.accuracy * 0.7
-      name_target_en = "по голове"
-    elsif target_en >= 4 and target_en <= 6
-      damage_en *= 0.7
-      accurasy_action_en = @enemy.accuracy * 1.5
-      name_target_en = "по ногам"
-    else
-      accurasy_action_en = @enemy.accuracy
-    end
-    #-----------------------------------------------------------------------------------------------------------
-    puts '-----------------------------------------------------------------------------------------'
-
-    # Расчет блока щитом --------------------------------------------------------------------------------------------
-    chanse_block_pl = rand(1..100)
-    if @hero.block_chance >= chanse_block_pl
-      damage_en /= @hero.block_power_coeff
-    end
-
-    chanse_block_en = rand(1..100)
-    if @enemy.block_chance >= chanse_block_en
-      damage_pl /= @enemy.block_power_coeff
-    end
-    # ---------------------------------------------------------------------------------------------------------------
-
-    # Расчет итогового урона-----------------------------------------------------------------------------------------
-    damage_pl -= @enemy.armor
-    if damage_pl < 0
-      damage_pl = 0
-    end
-
-    damage_en -= @hero.armor
-    if damage_en < 0
-      damage_en = 0
-    end
-    #----------------------------------------------------------------------------------------------------------------
-
-    # Расчет попадания/промаха и проведения атак и навыков -----------------------------------------------------
-    if accuracy_action_pl >= rand(1..100)
-      puts "#{@enemy.name} заблокировал #{@enemy.block_power_in_percents}% урона" if @enemy.block_chance >= chanse_block_en
-      @enemy.hp -= damage_pl
-      puts "Вы нанесли #{damage_pl.round} урона #{target_name_pl}"
-      hit_miss_pl = 1
-    else
-      puts "Вы промахнулись #{target_name_pl}"
-      hit_miss_pl = 0
-    end
-
-    case @hero.passive_skill.name
-    when "Ошеломление"
-      if hit_miss_pl == 1 and damage_pl * @hero.passive_skill.accuracy_reduce_coef > (@enemy.hp + damage_pl) / 2 # прибавляется дамаг который отнялся выше
-        accurasy_action_en *= 0.1*rand(1..9)
-        puts "атака ошеломила врага, уменьшив его точность до #{(@enemy.accuracy * 0.1 * rand(1..9)).round}"
-      end
-    when "Концентрация"
-      if hit_miss_pl == 1 and @hero.passive_skill.damage_bonus > 0
-        damage_bonus = @hero.passive_skill.damage_bonus # чтобы в след 2х строках был одинаковый
-        @enemy.hp -= damage_bonus
-        puts "дополнительный урон от концентрации #{damage_bonus.round(1)}"
-      end
-    end
-
-    if accurasy_action_en >= rand(1..100)
-      puts "Вы заблокировали #{@hero.block_power_in_percents}% урона" if @hero.block_chance >= chanse_block_pl
-      @hero.hp -= damage_en
-      puts "#{@enemy.name} нанес #{damage_en.round} урона #{name_target_en}"
-      hit_miss_en = 1
-    else
-      puts "#{@enemy.name} промахнулся #{name_target_en}"
-      hit_miss_en = 0
-    end
-    #------------------------------------------------------------------------------------------------------------------
-
-    @hero.regeneration_hp_mp # регенерация
-
-    # Результат обмена ударами --------------------------------------------------------------------------------
-    if @hero.hp > 0 and @enemy.hp > 0
-      puts "У вас осталось #{@hero.hp.round}/#{@hero.hp_max} жизней и #{@hero.mp.round}/#{@hero.mp_max} выносливости, у #{@enemy.name}а осталось #{@enemy.hp.round} жизней."
-    elsif @hero.hp > 0 and @enemy.hp <= 0
-      puts "У вас осталось #{@hero.hp.round}/#{@hero.hp_max} жизней и #{@hero.mp.round}/#{@hero.mp_max} выносливости, у #{@enemy.name}а осталось #{@enemy.hp.round} жизней."
-      puts "#{@enemy.name} убит, победа!!!"
-    elsif @hero.hp <= 0
-      puts "Ты убит - слабак!"
-      Art.display_art(:game_over)
-      exit
-    end
-    #------------------------------------------------------------------------------------------------------------------
-
-    # Побег ---------------------------------------------------------------------------------------------------
-    if @hero.hp < (@hero.hp_max * 0.15) and @hero.hp > 0 and @enemy.hp > 0
-      print 'Ты на пороге смерти. Чтобы убежать введи Y : '
-      run_select = gets.strip.upcase
-      if run_select == 'Y'
-        run_chance = rand(0..2)
-        if run_chance >= 1
-          puts "Сбежал ссыкло, штраф 5 опыта"
-          @hero.exp -= 5
-          run = true
-        else
-          @hero.hp -= damage_en
-          puts "Не удалось убежать #{@enemy.name} нанес #{damage_en.round} урона"
-          if @hero.hp <= 0
-            puts "Ты убит - трусливая псина!"
-            Art.display_art(:game_over)
-            exit
-          end
-          run = false
-        end
-      end
-    end
-    #-----------------------------------------------------------------------------------------------------------------
+    round = AttacksRound.new(@hero, @enemy)
+    round.action
+    run = round.hero_run?
 
     lap += 1 # номер хода
-
   end
-  #===================================================================================================================
 
   puts '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
 
-  # Сбор лута-----------------------------------------------------------------------------------------------------
+  # Сбор лута
   if run == false
     EnemyLoot.new(@hero, @enemy).looting
     FieldLoot.new(@hero).looting
     SecretLoot.new(@hero).looting
   end
-  #-------------------------------------------------------------------------------------------------------------
 
-  @hero.add_exp_and_hero_level_up(@enemy.exp_gived) # Получение опыта и очков
+  @hero.add_exp_and_hero_level_up(@enemy.exp_gived) if !run # Получение опыта и очков
 
   puts '-------------------------------------------------------------------------------------------------'
   leveling += 1
 end
-#====================================================================================================================
 
 
 
