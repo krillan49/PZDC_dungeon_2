9c5730689a59f365465994c1f0c986ad965d8520
krillan <pushkinidze49@gmail.com>
2024-08-19 19:50:28 +0300

    some updates

--- services/loot/enemy_loot.rb
+++ services/loot/enemy_loot.rb
@@ -18,38 +18,38 @@ class EnemyLoot
   private
 
   def weapon_loot
-    @messages.main = "Having searched the body #{@enemy.name} you found #{@enemy.weapon.name}"
+    @messages.main = "After searching the #{@enemy.name}'s body you found #{@enemy.weapon.name}"
     display_loot_screen(:weapon)
     @hero.weapon = @enemy.weapon if gets.strip.upcase == 'Y'
   end
 
   def body_armor_loot
-    @messages.main = "Having searched the body #{@enemy.name} you found #{@enemy.body_armor.name}"
+    @messages.main = "After searching the #{@enemy.name}'s body you found #{@enemy.body_armor.name}"
     display_loot_screen(:body_armor)
     @hero.body_armor = @enemy.body_armor if gets.strip.upcase == 'Y'
   end
 
   def head_armor_loot
-    @messages.main = "Having searched the body #{@enemy.name} you found #{@enemy.head_armor.name}"
+    @messages.main = "After searching the #{@enemy.name}'s body you found #{@enemy.head_armor.name}"
     display_loot_screen(:head_armor)
     @hero.head_armor = @enemy.head_armor if gets.strip.upcase == 'Y'
   end
 
   def arms_armor_loot
-    @messages.main = "Having searched the body #{@enemy.name} you found #{@enemy.arms_armor.name}"
+    @messages.main = "After searching the #{@enemy.name}'s body you found #{@enemy.arms_armor.name}"
     display_loot_screen(:arms_armor)
     @hero.arms_armor = @enemy.arms_armor if gets.strip.upcase == 'Y'
   end
 
   def shield_loot
-    @messages.main = "Having searched the body #{@enemy.name} you found #{@enemy.shield.name}"
+    @messages.main = "After searching the #{@enemy.name}'s body you found #{@enemy.shield.name}"
     display_loot_screen(:shield)
     @hero.shield = @enemy.shield if gets.strip.upcase == 'Y'
   end
 
   def coins_loot
     @hero.coins += @enemy.coins_gived
-    @messages.log << "Having searched the body #{@enemy.name} you found #{@enemy.coins_gived} coins. Now you have #{@hero.coins} coins"
+    @messages.log << "After searching the #{@enemy.name}'s body you found #{@enemy.coins_gived} coins. Now you have #{@hero.coins} coins"
     @messages.main = "Мy precious... Press Eenter to continue"
     display_message_screen
     gets
